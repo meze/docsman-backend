3522b634eba67b591ef410e7724781b47043c0ba
Evgeniy Z <mz.evgeny@gmail.com>
2017-03-24 01:34:25 +0300

    Fix formatting

--- app/Resources/views/base.html.php
+++ app/Resources/views/base.html.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <title><?php $view['slots']->output('title', 'Docsman') ?></title>
+        <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
         <?php $view['slots']->output('stylesheets') ?>
         <link rel="icon" type="image/x-icon" href="<?php echo $view['assets']->getUrl('favicon.ico') ?>" />
     </head>
--- app/Resources/views/default/index.html.php
+++ app/Resources/views/default/index.html.php
@@ -6,6 +6,6 @@
 
 <?php $view['slots']->start('javascripts') ?>
     <script type="text/javascript" src="<?php echo $view['assets']->getUrl('/react/survive/node_modules/babel-polyfill/dist/polyfill.js') ?>"></script>
-<script type="text/javascript" src="http://localhost:9000/vendor_bundle.js"></script>
-<script type="text/javascript" src="http://localhost:9000/app_bundle.js"></script>
+    <script type="text/javascript" src="http://localhost:9000/vendor_bundle.js"></script>
+    <script type="text/javascript" src="http://localhost:9000/app_bundle.js"></script>
 <?php $view['slots']->stop() ?>
